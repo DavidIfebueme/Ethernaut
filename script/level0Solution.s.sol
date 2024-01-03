2afd65b5e9c63610055e7e31116945af7ab51fc3
DavidIfebueme <davidifebueme@gmail.com>
2024-01-03 15:27:39 +0100

    Fixed the foundry.toml bug by adding the sepolia rpc

--- script/level0Solution.s.sol
+++ script/level0Solution.s.sol
@@ -2,18 +2,18 @@
 pragma solidity ^0.8.0;
 
 
-import "../src/level0.sol";
-import "forge-std/console.sol";
+import "../src/Level0.sol";
 import "forge-std/Script.sol";
+import "forge-std/console.sol";
 
-contract level0Solution is Script{
+contract Level0Solution is Script {
     Level0 public level0 = Level0(0xc96da25643f31a480F5aF32E7ee4a1E675D2dd49);
 
     function run() external{
         string memory password = level0.password();
-        console.log(password);
-        vm.startBroadcast(vm.envUint("PRIVATE_ADDRESS"));
+        console.log("Password: ", password);
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
         level0.authenticate(password);
         vm.stopBroadcast();
-    }
+    } 
 }
